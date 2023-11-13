976f511322ed1205cfe847c50393a58f65cbed26
AndresMauricio <52362802+AndresMauricioB@users.noreply.github.com>
2023-11-13 18:19:57 -0500

    Update aj.py

--- aj.py
+++ aj.py
@@ -2,29 +2,34 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# My first app
-Hello *world!*
+# APP
+Hola Mundo*
 """)
+# Datos de ejemplos
 data = {
     'Nombre': ['Alice', 'Bob', 'Charlie', 'David'],
     'Edad': [25, 30, 35, 28],
     'Ciudad': ['Nueva York', 'Los Ángeles', 'Chicago', 'Houston']
 }
-
 df = pd.DataFrame(data)
+# Este comando crea un gráfico de línea a partir de los datos contenidos en un DataFrame de Pandas (df)
 st.line_chart(df)
 
+# Este comando genera un gráfico de barras a partir de los 
+# datos contenidos en un DataFrame de Pandas (df).
 st.bar_chart(df)
 
 
-# Gráfico de barras de edades
+# Este comando crea un gráfico de barras utilizando solo la columna 'Nombre' como 
+# etiquetas en el eje x y la columna 'Edad' como valores en el eje y.
 st.bar_chart(df.set_index('Nombre')['Edad'])
 
-# Gráfico de dispersión de edades por ciudad
+# Este comando genera un gráfico de dispersión que muestra puntos dispersos en función de los 
+# valores de las columnas 'Ciudad' en el eje x y 'Edad' en el eje y.
 st.scatter_chart(df, x='Ciudad', y='Edad')
 
-# Gráfico de torta de edades
-st.pie_chart(df['Edad'])
+# Este comando crea un gráfico de torta o pastel
+st.pie_chart(df)
  
 
 
